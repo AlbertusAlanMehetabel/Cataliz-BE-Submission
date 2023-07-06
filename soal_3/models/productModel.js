547544a1796b8442a064d9b76bdb170b774b11e3
Albertus Alan Mehetabel <albert.alan02@gmail.com>
2023-07-06 22:31:16 +0700

    Add productModel.js

---
+++ soal_3/models/productModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// Skema untuk model produk
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+// Export dan buat model
+module.exports = mongoose.model('Product', productSchema);
