4683fa7a818be4b0d146b184d31598c484dc1425
Albertus Alan Mehetabel <albert.alan02@gmail.com>
2023-07-06 22:31:36 +0700

    Add productRepository.js

---
+++ soal_3/repositories/productRepository.js
@@ -0,0 +1,26 @@
+const Product = require('../models/productModel');
+
+// Operasi CRUD di databse
+class ProductRepository {
+  async getAllProducts() {
+    return await Product.find();
+  }
+
+  async getProductById(id) {
+    return await Product.findById(id);
+  }
+
+  async createProduct(productData) {
+    return await Product.create(productData);
+  }
+
+  async updateProduct(id, productData) {
+    return await Product.findByIdAndUpdate(id, productData, { new: true });
+  }
+
+  async deleteProduct(id) {
+    return await Product.findByIdAndDelete(id);
+  }
+}
+
+module.exports = ProductRepository;
